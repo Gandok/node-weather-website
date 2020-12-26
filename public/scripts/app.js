c0ce5e58b0d7d8ed322bc76cb4f373d3c81526ca
Gandok <cabreradelacruz.carlos@gmail.com>
2020-12-26 01:23:32 -0600

    setup para deploy en heroku

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,7 @@ const weatherMsg = document.querySelector('#weather-msg')
 const errorMsg = document.querySelector('#error-msg')
 
 const getWeather = location => {
-    const url = 'http://localhost:3000/weather?address=' + location
+    const url = '/weather?address=' + location
     fetch(url).then((response) => {
     response.json().then((data) => {
         if(data.error){
